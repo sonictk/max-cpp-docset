36596193674c69f4ccfeee4b9706d3c4b56b6cec
Siew Yi Liang <admin@sonictk.com>
2017-06-18 03:02:16 -0700

    fixing broken links to stylesheets

--- bin/clean_html_documentation.py
+++ bin/clean_html_documentation.py
@@ -18,7 +18,6 @@ def format_files(all_files, docs_path, output_path):
     writes the output files to a subdirectory.
     """
     logger = logging.getLogger(__name__)
-    # NOTE (sonictk): Copy over the necessary resource files first
 
     for i, f in enumerate(all_files):
         if os.path.splitext(f)[-1] != '.html':
@@ -27,23 +26,26 @@ def format_files(all_files, docs_path, output_path):
         html = open(os.path.join(docs_path, f)).read()
         soup = BeautifulSoup(html, 'html.parser')
         
+        for img in soup.find_all('img'):
+            src = img.get('src')
+            if src:
+                img['src'] = src.replace('cpp_ref/', './')
+
         for script in soup.find_all('script'):
             src = script.get('src')
-            if src and 'www.microsofttranslator.com' in src:
-                script['src'] = ''
+            if src:
+                if 'www.microsofttranslator.com' in src:
+                    script['src'] = ''
+                script['src'] = src.replace('../scripts', './scripts')
         
         links = soup.find_all('a') + soup.find_all('link')
 
         for link in links:
             href = link.get('href')
             if href: 
-                if '#!/url=./cpp_ref/' in href:
-                    new_href = href.replace('#!/url=./cpp_ref/', './')
-                elif 'cpp_ref/' in href:
-                    new_href = href.replace('cpp_ref/', './')
-                else:
-                    continue
-                link['href'] = new_href
+                link['href'] = href.replace('style/', './')\
+                                   .replace('#!/url=./cpp_ref/', './')\
+                                   .replace('cpp_ref/', './')
 
         # Also insert anchors in order for table of contents to work
         for td in soup.find_all('td'):
@@ -67,17 +69,29 @@ def format_files(all_files, docs_path, output_path):
 
 
 def main(docs_sources, output_path, multi_thread=False, max_version='2017'):
-    """This is the main entry point of the program."""
+    """
+    This is the main entry point of the program.
 
+    :param docs_sources: ``str`` path to the original documentation sources. This 
+        should contain the ``cpp_ref`` folder, among other resources.
+    
+    :param output_path: ``str`` path to write the formatted HTML files to.
+
+    :param multi_thread: ``bool`` to indicate if multithreading support should be 
+        enabled.
+
+    :param max_version: ``str`` indicating what version of 3ds max the docset 
+        being generated for is.
+    """
     logger = logging.getLogger(__name__)
     if not docs_sources:
-        sources_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+        docs_sources = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
                                     'resources',
                                     max_version,
                                     'cpp_ref')
     if not output_path:
-        output_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 
-                                'max-{0}-cpp.docset',format(max_version),
+        output_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 
+                                'max-{0}-cpp.docset'.format(max_version),
                                 'Contents',
                                 'Resources',
                                 'Documents')
@@ -89,12 +103,27 @@ def main(docs_sources, output_path, multi_thread=False, max_version='2017'):
     logger.info('Formatting documentation...')
 
     if os.path.isdir(output_path):
-        logger.debug('Removing existing directory: {0}...'.format(output_path))
+        logger.debug('Removing existing directory: {0}!'.format(output_path))
         shutil.rmtree(output_path)
     os.makedirs(output_path)
 
+    # NOTE (sonictk): Copy over the necessary resource files first
+    styles_path = os.path.join(os.path.dirname(docs_sources), 'style')
+    if not os.path.isdir(styles_path):
+        raise IOError('The CSS styles directory: {0} does not exist!'.format(styles_path))
+    [shutil.copy(os.path.join(styles_path, css), output_path) for css in os.listdir(styles_path)]
+
+    scripts_path = os.path.join(os.path.dirname(docs_sources), 'scripts')
+    if not os.path.isdir(scripts_path):
+        raise IOError('The JScript directory: {0} does not exist!'.format(scripts_path))
+    shutil.copytree(scripts_path, os.path.join(output_path, 'scripts'))
+
     all_files = os.listdir(docs_sources)
     logger.debug('Total number of files to process: {0}'.format(len(all_files)))
+    for f in all_files:
+        if os.path.splitext(f)[-1] != '.html':
+            # NOTE (sonictk): Just copy it over anyway, since those files are needed (CSS, scripts etc.)
+            shutil.copy(os.path.join(docs_sources, f), output_path)
     jobs = []
     if multi_thread:
         for s in chunk(all_files, 500):
@@ -122,7 +151,7 @@ if __name__ == '__main__':
                         '--maxVersion',
                         type=str,
                         help='The 3ds max version of the docset to generate.',
-                        required=True)
+                        default='2017')
     parser.add_argument('-mt',
                         '--multiThread',
                         default=False,
